a75146d58b4dc0a688fd2aa2f20a6a1ae05c5c2b
tranquangvu <vutran.513@gmail.com>
2018-09-05 03:10:31 +0700

    Init package

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        include: path.join(__dirname, 'src'),
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              babelrc: false,
+              presets: [['env', { modules: false }], 'react', 'stage-0']
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
